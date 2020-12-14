e121e1535deffc7e1edd2c388e25c2fb091066ca
phantomramen <ali.hernandez9two@gmail.com>
2020-12-13 19:02:21 -0700

    different

--- server.js
+++ server.js
@@ -49,8 +49,9 @@ app.use(passport.session());
 app.use(routes);
 
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
